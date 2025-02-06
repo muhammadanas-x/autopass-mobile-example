da2d191bc975e501c8652d414f12c9e5314b1871
Sean Zellmer <sean@lejeunerenard.com>
2025-02-06 16:38:33 -0600

    Revert "Lint files w/ prettier & remove standard dev dependency"
    This reverts commit a6a9ab1c9a6637aeb00b8eda35a188ddcd977c3b.

--- backend/backend.mjs
+++ backend/backend.mjs
@@ -1,45 +1,43 @@
 // /* global Bare, BareKit */
 
-import RPC from "bare-rpc";
-import fs from "bare-fs";
+import RPC from 'bare-rpc'
+import fs from 'bare-fs'
 
-import Autopass from "autopass";
-import Corestore from "corestore";
-const { IPC } = BareKit;
+import Autopass from 'autopass'
+import Corestore from 'corestore'
+const { IPC } = BareKit
 
-const path =
-  Bare.argv[0] === "android"
-    ? "/data/data/to.holepunch.bare.expo/autopass-example"
-    : "./tmp/autopass-example/";
+const path = Bare.argv[0] === 'android' ? '/data/data/to.holepunch.bare.expo/autopass-example' : './tmp/autopass-example/'
 
 const rpc = new RPC(IPC, (req, error) => {
   // Handle two way communication here
-});
+})
 
 // For a clean start
 if (fs.existsSync(path)) {
   fs.rmSync(path, {
     recursive: true,
-    force: true,
-  });
+    force: true
+  })
 }
 
-fs.mkdirSync(path);
+fs.mkdirSync(path)
 
-const invite = Bare.argv[1];
-const pair = Autopass.pair(new Corestore(path), invite);
+const invite = Bare.argv[1]
+const pair = Autopass.pair(new Corestore(path), invite)
 
-const pass = await pair.finished();
-await pass.ready();
+const pass = await pair.finished()
+await pass.ready()
 
-pass.on("update", async (e) => {
-  const req = rpc.request("reset");
-  req.send("data");
+pass.on('update', async (e) => {
+  const req = rpc.request('reset')
+  req.send('data')
 
   for await (const data of pass.list()) {
-    if (data.value[0] === "password") {
-      const req = rpc.request("message");
-      req.send(JSON.stringify(data.value));
+    if (data.value[0] === 'password') {
+      const req = rpc.request('message')
+      req.send(JSON.stringify(data.value))
     }
   }
-});
+})
+
