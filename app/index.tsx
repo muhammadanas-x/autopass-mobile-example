81e5024aef7217847d0923c0422ca6226d05da85
Sean Zellmer <sean@lejeunerenard.com>
2025-02-06 14:40:28 -0600

    Remove unused arg when reseting data list

--- app/index.tsx
+++ app/index.tsx
@@ -44,7 +44,7 @@ export default function App () {
           }
 
           if (req.command === 'reset') {
-            setDataList((prevDataList) => [])
+            setDataList(() => [])
           }
         })
       })
